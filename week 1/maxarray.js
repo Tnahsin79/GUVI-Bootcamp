5f1542fbede4307ebcd88d56bc24953002f229a5
Nishant Sharma <36157944+Tnahsin79@users.noreply.github.com>
2020-07-21 18:09:09 +0530

    Create maxarray.js

---
+++ week 1/maxarray.js
@@ -0,0 +1,7 @@
+  var array=[4,7,12,78,55,38,2,97];
+  var max,i;
+  max=array[0];
+  for(i=0;i<array.length;i++)
+  if(array[i]>max)
+  max=array[i];
+  console.log("the maximum element is: "+max);
