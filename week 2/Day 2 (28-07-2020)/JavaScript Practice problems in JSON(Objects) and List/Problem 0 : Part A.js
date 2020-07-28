0adf410a79c789d6b56f249f1d01af41e9df738d
Nishant Sharma <36157944+Tnahsin79@users.noreply.github.com>
2020-07-28 13:30:44 +0530

    Update Problem 0 : Part A.js

--- week 2/Day 2 (28-07-2020)/JavaScript Practice problems in JSON(Objects) and List/Problem 0 : Part A.js
+++ week 2/Day 2 (28-07-2020)/JavaScript Practice problems in JSON(Objects) and List/Problem 0 : Part A.js
@@ -15,4 +15,28 @@ var cat = {
  }
  ]
 }
+//console.log(cat);
+
+cat["height"]=7;
+cat["weight"]=9;
+cat["name"]='Fluffyy';
+var act=cat["catFriends"];
+var sow=0;
+var i;
+var l=act.length;
+//console.log(l);
+for(i=0;i<l;i++)
+{
+	console.log(act[i]["name"]);
+	console.log(act[i]["activities"]);
+	sow+=a[i]["weight"];
+}
+console.log("sum of weight =",sow);
+for(i=0;i<cat["catFriends"].length;i++)
+{
+	cat["catFriends"][i]["activities"].push('activity1');
+	cat["catFriends"][i]["activities"].push('activity2');
+}
+cat["catFriends"][0]["furcolor"]='pink';
+
 console.log(cat);
