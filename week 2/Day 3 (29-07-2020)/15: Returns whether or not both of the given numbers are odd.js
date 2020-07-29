da56aa0f2c6c3ba8655d21867ea4d71950b8c1be
Nishant Sharma <36157944+Tnahsin79@users.noreply.github.com>
2020-07-29 17:03:42 +0530

    Create 15: Returns whether or not both of the given numbers are odd.js

---
+++ week 2/Day 3 (29-07-2020)/15: Returns whether or not both of the given numbers are odd.js
@@ -0,0 +1,11 @@
+function areBothOdd(num1, num2){
+ // your code here
+ if(num1%2===1 && num2%2===1)
+ return true;
+ else
+ return false;
+}
+console.log(areBothOdd(1, 3));
+console.log(areBothOdd(1, 4));
+console.log(areBothOdd(2, 3));
+console.log(areBothOdd(0, 0));
