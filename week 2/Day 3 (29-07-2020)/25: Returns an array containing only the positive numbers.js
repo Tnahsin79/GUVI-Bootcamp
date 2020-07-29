1885c51a71d2c49a3e1305cec2dabd3f4e647412
Nishant Sharma <36157944+Tnahsin79@users.noreply.github.com>
2020-07-29 19:59:25 +0530

    Update 25: Returns an array containing only the positive numbers.js

--- week 2/Day 3 (29-07-2020)/25: Returns an array containing only the positive numbers.js
+++ week 2/Day 3 (29-07-2020)/25: Returns an array containing only the positive numbers.js
@@ -1,1 +1,16 @@
-
+function getPositives(ar)
+{
+ // your code here
+ let i;
+ //console.log(ar);
+ for(i=0;i<ar.length;i++)
+ {
+     if(ar[i]<0)
+     ar.splice(i,1);
+     //i--;
+ }
+ return ar;
+}
+var ar = [-5, 10, -3, 12, -9, 5, 90, 0, 1];
+var ar2 = getPositives(ar);
+console.log(ar2);
