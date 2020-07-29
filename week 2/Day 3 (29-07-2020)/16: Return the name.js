17f9f234686ac8e921de91bceb7b916498ab0620
Nishant Sharma <36157944+Tnahsin79@users.noreply.github.com>
2020-07-29 17:15:23 +0530

    Create 16: Return the name.js

---
+++ week 2/Day 3 (29-07-2020)/16: Return the name.js
@@ -0,0 +1,11 @@
+function getFullName(firstName, lastName){
+ // your cod(e here
+ if(firstName===undefined)
+ firstName="";
+ if(lastName===undefined)
+ lastName="";
+ console.log(firstName,lastName);
+}
+getFullName("GUVI", "GEEK");
+getFullName("GUVI", );
+getFullName("", "");
