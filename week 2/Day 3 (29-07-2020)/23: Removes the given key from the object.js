a8ce6c2c298cb16daf8e3b66602235d088b9868a
Nishant Sharma <36157944+Tnahsin79@users.noreply.github.com>
2020-07-29 19:41:51 +0530

    Update 23: Removes the given key from the object.js

--- week 2/Day 3 (29-07-2020)/23: Removes the given key from the object.js
+++ week 2/Day 3 (29-07-2020)/23: Removes the given key from the object.js
@@ -1,1 +1,11 @@
-
+var obj = {
+ mykey: "value"
+};
+function addProperty(obj, key){
+ // your code here
+ obj[key]=true;
+ console.log(obj);
+ delete obj[key];
+ console.log(obj);
+}
+addProperty(obj, "mykey2");
