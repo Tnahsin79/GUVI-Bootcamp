9397105b31ed0f683f0af1d8e3524bebb70867a8
Nishant Sharma <36157944+Tnahsin79@users.noreply.github.com>
2020-07-29 19:55:09 +0530

    Update 24: Return an array.js

--- week 2/Day 3 (29-07-2020)/24: Return an array.js
+++ week 2/Day 3 (29-07-2020)/24: Return an array.js
@@ -1,1 +1,17 @@
-
+var arr = [-5, 10, -3, 12, -9, 5, 90, 0, 1];
+function countPositivesSumNegatives(arr) {
+    let i,c=0,s=0;
+    let a=[];
+ for(i=0;i<arr.length;i++)
+ {
+     if(arr[i]>=0)
+     c++;
+     else
+     s+=arr[i];
+ }
+ a.push(c);
+ a.push(s);
+ console.log(a);
+ //return a;
+}
+countPositivesSumNegatives(arr);
