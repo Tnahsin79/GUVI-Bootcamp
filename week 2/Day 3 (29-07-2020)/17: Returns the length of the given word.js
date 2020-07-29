2367c4ea181c1d4c51210f6e6f0e7e54e1d7d5d8
Nishant Sharma <36157944+Tnahsin79@users.noreply.github.com>
2020-07-29 17:23:42 +0530

    Create 17: Returns the length of the given word.js

---
+++ week 2/Day 3 (29-07-2020)/17: Returns the length of the given word.js
@@ -0,0 +1,11 @@
+function getLengthOfWord(word1){
+ // your code here
+ if(typeof(word1)==="string")
+ console.log(word1.length);
+ else
+ console.log("-1")
+}
+getLengthOfWord("GUVI");
+getLengthOfWord("");
+getLengthOfWord();
+getLengthOfWord(9);
