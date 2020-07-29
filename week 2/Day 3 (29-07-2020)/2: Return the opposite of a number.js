8fb869ad94cd52266d1f4b9f90e1270c74fe1242
Nishant Sharma <36157944+Tnahsin79@users.noreply.github.com>
2020-07-29 13:35:12 +0530

    Create 2: Return the opposite of a number.js

---
+++ week 2/Day 3 (29-07-2020)/2: Return the opposite of a number.js
@@ -0,0 +1,14 @@
+function getOpposite(n) {
+ if(n===0)
+ console.log(n);
+ else
+ if(Number.isInteger(n))
+ console.log(n*(-1));
+ else
+ console.log("-1");
+}
+getOpposite(5);
+getOpposite(0);
+getOpposite(-5);
+getOpposite("5a");
+getOpposite(5.5);
