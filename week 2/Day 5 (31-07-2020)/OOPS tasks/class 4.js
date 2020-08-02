6e393172a7145eafcbc5da1c0d7fad6aa3849d81
Nishant Sharma <36157944+Tnahsin79@users.noreply.github.com>
2020-08-02 06:28:25 +0530

    Create class 4.js

---
+++ week 2/Day 5 (31-07-2020)/OOPS tasks/class 4.js
@@ -0,0 +1,50 @@
+class Bank{
+    var code;
+    var address;
+    function manages(){}
+    function maintains(){}
+}
+
+class atm{
+    var location;
+    var managedby;
+    function identifies(){}
+    function transactions(){}
+}
+
+class customer{
+    var name;
+    var dob;
+    var address;
+    var card_number;
+    var pin;
+    function verifyPassword(){}
+} 
+
+class account{
+    var number;
+    var balance;
+    function deposit(){}
+    function withdraw(){}
+    function create_transactons(){}
+}
+
+class atm_transactions{
+    var transaction_id;
+    var date;
+    var type;
+    var amount;
+    var postbalance;
+    function modifies(){}
+}
+
+class currentAccount{
+    var accountnumber;
+    var balance;
+    function withdraw(){}
+}
+
+class currentAccount{
+    var accountnumber;
+    var balance;
+}
