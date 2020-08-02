9457a5571d12519518bbbe93dd21445a36d48590
Nishant Sharma <36157944+Tnahsin79@users.noreply.github.com>
2020-08-02 22:30:58 +0530

    Create task14.js

---
+++ week 2/Day 5 (31-07-2020)/Call Apply Bind tasks/task14.js
@@ -0,0 +1,15 @@
+var person = {
+    name : "Jack",
+    prop : {
+        name : "Daniel",
+        getName : function() {
+            return this.name;
+        }
+    }
+};
+
+var name = person.prop.getName.bind(person);
+console.log(name);
+
+var  name = person.prop.getName();
+console.log(name);
