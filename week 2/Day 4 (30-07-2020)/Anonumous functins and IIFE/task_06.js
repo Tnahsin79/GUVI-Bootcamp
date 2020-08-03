8a07020ada1f6c884766997acc7e2bb292081823
Nishant Sharma <36157944+Tnahsin79@users.noreply.github.com>
2020-08-03 09:33:25 +0530

    Update task_06.js

--- week 2/Day 4 (30-07-2020)/Anonumous functins and IIFE/task_06.js
+++ week 2/Day 4 (30-07-2020)/Anonumous functins and IIFE/task_06.js
@@ -25,3 +25,29 @@ var prime = function(c,d){
     console.log(m1,m2);
 };
 prime(a,b);
+
+var prime2 = (function(){
+    
+    a.sort(function(x,y){
+        return x-y;
+    });
+    
+    b.sort(function(x,y){
+        return x-y;
+    });
+    
+    let m1,m2,i;
+    if(a.length%2===1)
+    {
+        i=parseInt(a.length/2);
+        m1=a[i];m2=b[i];
+    }
+    else
+    {
+        i=parseInt(a.length/2);
+        m1=(a[i]+a[i-1])/2;
+        m1=(b[i]+b[i-1])/2;
+    }
+    return [m1,m2];
+};
+console.log(prime2);
