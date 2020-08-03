abe1e573e0650841dacd3591317101ae9609db37
Nishant Sharma <36157944+Tnahsin79@users.noreply.github.com>
2020-08-03 09:22:04 +0530

    Update task_01.js

--- week 2/Day 4 (30-07-2020)/Anonumous functins and IIFE/task_01.js
+++ week 2/Day 4 (30-07-2020)/Anonumous functins and IIFE/task_01.js
@@ -1,5 +1,5 @@
 a=[34,1,26,7,9,66,21,19,5];
-var odd = function(d){
+var odd = function(d){  //anonymous function
     let i,o=[];
     for(i=0;i<d.length;i++)
     if(d[i]%2===1)
@@ -8,7 +8,7 @@ var odd = function(d){
 };
 console.log(odd(a));
 
-var odd2 = (function(){
+var odd2 = (function(){   //IIFE
     let i,o=[];
     for(i=0;i<a.length;i++)
     if(a[i]%2===1)
